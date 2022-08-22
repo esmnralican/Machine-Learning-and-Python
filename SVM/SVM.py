2d07b5ae1f84d34f83c6ec3d9a1b6c1a105f0f48
ledger <76178128+esmnralican@users.noreply.github.com>
2022-08-22 16:55:05 +0300

    Add files via upload

---
+++ SVM/SVM.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 12:48:36 2022
+
+@author: esmnralican
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#%%
+data = pd.read_csv("data.csv")
+#%%
+data.drop(["id" , "Unnamed: 32"] , axis = 1 , inplace= True )
+
+M = data[data.diagnosis == "M"]
+B = data[data.diagnosis == "B"]
+
+#%%
+# scatter plot
+plt.scatter(M.radius_mean , M.texture_mean , color = "red", label="kotu")
+plt.scatter(B.radius_mean , B.texture_mean , color = "green", label="iyi")
+plt.xlabel("radius_mean")
+plt.ylabel("texture_mean")
+plt.show()
+
+#%%data.diagnosis = [1 if each == "M" else 0 for each in data.diagnosis]
+x_data = data.drop(["diagnosis"], axis=1)   # features
+y = data.diagnosis.values    # labels
+
+#%%
+# Normalization
+x = (x_data - np.min(x_data)) / (np.max(x_data) - np.min(x_data)) 
+
+#%%
+# train - test split
+from sklearn.model_selection import train_test_split
+x_train , x_test , y_train , y_test = train_test_split(x , y , test_size = 0.3 , random_state=42)
+
+#%%
+# KNN model
+from sklearn.svm import SVC
+svm = SVC(random_state = 42) 
+svm.fit(x_train , y_train)
+#%%
+print(" accuracy of svm algorithm ", svm.score(x_test , y_test))
+
+
